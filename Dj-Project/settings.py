33e83a5690e08b4eba961e70bbdd9a3158b38165
Rian1010 <rtegally1098@gmail.com>
2020-05-07 02:50:47 +0200

    Added a new app for the phone products

--- Dj-Project/settings.py
+++ Dj-Project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_forms_bootstrap',
     'accounts',
+    'phoneShop'
 ]
 
 MIDDLEWARE = [
@@ -82,6 +83,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -138,6 +140,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
--- Dj-Project/urls.py
+++ Dj-Project/urls.py
@@ -17,9 +17,16 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from accounts.views import index
 from accounts import urls as accounts_urls
+from phoneShop import urls as url_products
+from phoneShop.views import all_products
+from django.views import static
+from .settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', index, name="index"),
+    url(r'^$', all_products, name="index"),
+    # url(r'^$', index, name="index"),
     url(r'^accounts/', include(accounts_urls)),
-]
+    url(r'^products/', include(url_products)),
+    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
+    ]
