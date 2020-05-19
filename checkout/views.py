59e580bbd571cbdb6920884130fdf8476b0f4e87
Rian1010 <rtegally1098@gmail.com>
2020-05-20 00:52:44 +0200

    Solved a stripe bug and styled messages

--- checkout/views.py
+++ checkout/views.py
@@ -9,34 +9,36 @@ from phoneShop.models import Product
 import stripe
 
 # Create your views here.
-stripe.api_key = settings.STRIPE_SECRET
+stripe.api_key=settings.STRIPE_SECRET
 
 
 @login_required()
 def checkout(request):
-    if request.method == "POST":
-        order_form = OrderForm(request.POST)
-        payment_form = MakePayment(request.POST)
+    if request.method=="POST":
+        order_form=OrderForm(request.POST)
+        payment_form=MakePayment(request.POST)
 
         if order_form.is_valid() and payment_form.is_valid():
-            order = order_form.save(commit=False)
-            order.date = timezone.now()
+            order=order_form.save(commit=False)
+            order.date=timezone.now()
             order.save()
 
-            cart = request.session.get('cart', {})
-            total = 0
+            cart=request.session.get('shopping_cart', {})             
+            total=0
             for id, quantity in cart.items():
-                product = get_object_or_404(Product, pk=id)
+                product=get_object_or_404(Product, pk=id)
                 total += quantity * product.price
-                order_line_item = OrderLineItem(
+                order_line_item=OrderLineItem(
                     order=order,
                     product=product,
-                    quantity=quantity
+                    quantity=quantity,
+                    total=total,
+                    price=product.price,
                 )
                 order_line_item.save()
             try:
                 # Stripe takes everything in cents, therefore total * 100
-                customer = stripe.Charge.create(
+                customer=stripe.Charge.create(
                     amount=int(total * 100),
                     currency="EUR",
                     description=request.user.email,
@@ -46,16 +48,15 @@ def checkout(request):
                 messages.error(request, "Your card was declined")
             if customer.paid:
                 messages.error(request, "You have successfully paid")
-                request.session['cart'] = {}
-                return redirect(reverse('product'))
+                request.session['shopping_cart']={}
+                return redirect(reverse('profile'))
             else:
                 messages.error(request, "Unable to take payment")
         else:
-            print(payment_form.errors)
             messages.error(request, "We were unable to take a payment with that card")
 
     else:
-        payment_form = MakePayment()
-        order_form = OrderForm()
+        payment_form=MakePayment()
+        order_form=OrderForm()
 
     return render(request, "checkout.html", {"order_form": order_form, "payment_form": payment_form, "publishable": settings.STRIPE_PUBLISHABLE})
