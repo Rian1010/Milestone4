08dc0f0a3349d13aa49537a0b74c5296ab341688
Rian1010 <rtegally1098@gmail.com>
2020-06-01 11:21:25 +0200

    Made small changes

--- Dj-Project/settings.py
+++ Dj-Project/settings.py
@@ -169,7 +169,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
-MEDIAFILES_LOCATION = '/media/images/'
+MEDIAFILES_LOCATION = '/media/'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
